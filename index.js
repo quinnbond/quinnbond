eceed2c1f342917abc641c5e5dbccc38e3d3358c
quinnbond <97208874+quinnbond@users.noreply.github.com>
2023-04-15 19:00:38 -0600

    updated website with UTTU cover

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+
+let sound1 = new Audio("assets/Conga.wav");
+document.getElementById("spotify").addEventListener("click",function(){
+  sound2.play();
+})
+
+document.getElementById("applem").addEventListener("click",function(){
+    sound2.play();
+  })
+  
+
+document.getElementById("soundcloud").addEventListener("click",function(){
+sound2.play();
+})
+
+
+let sound2 = new Audio("assets/HouseHit.wav");
+
+document.getElementById("album").addEventListener("click",function(){
+  sound2.play();
+  })
+
+document.getElementById("album2").addEventListener("click",function(){
+  sound2.play();
+  })
+    
+
+document.getElementById("album3").addEventListener("click",function(){
+  sound2.play();
+  })
+
+document.getElementById("album4").addEventListener("click",function(){
+  sound2.play();
+  })
+
+document.getElementById("album5").addEventListener("click",function(){
+  sound2.play();
+  })
+
+  document.getElementById("album6").addEventListener("click",function(){
+    sound2.play();
+    })
